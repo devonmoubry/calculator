f517ad219be72337e7e95850ad6eeade38ced4fd
Devon Moubry <devon@moubry.com>
2017-03-08 21:55:48 -0600

    added operator buttons

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 var inputOne = document.querySelector('#number1');
 var inputTwo = document.querySelector('#number2');
-var submit = document.querySelector('#submit');
+var add = document.querySelector('#add');
+var subtract = document.querySelector('#subtract');
+var multiply = document.querySelector('#multiply');
+var divide = document.querySelector('#divide');
 var outputArea = document.querySelector('#output-area');
-submit.onclick = total;
+add.onclick = total;
+subtract.onclick = difference;
+multiply.onclick = product;
+divide.onclick = quotient;
+
 
 function total() {
   var valueOne = parseInt(inputOne.value);
   var valueTwo = parseInt(inputTwo.value);
   var answer = valueOne + valueTwo;
-
   outputArea.innerText = answer;
+  if (isNaN(answer) === true) {
+    alert('Please enter a number.');
+  }
+}
+  function difference() {
+    var valueOne = parseInt(inputOne.value);
+    var valueTwo = parseInt(inputTwo.value);
+    var answer = valueOne - valueTwo;
+    outputArea.innerText = answer;
+    if (isNaN(answer) === true) {
+      alert('Please enter a number.');
+    }
+}
 
+function product() {
+  var valueOne = parseInt(inputOne.value);
+  var valueTwo = parseInt(inputTwo.value);
+  var answer = valueOne * valueTwo;
+  outputArea.innerText = answer;
+  if (isNaN(answer) === true) {
+    alert('Please enter a number.');
+  }
+}
+
+function quotient() {
+  var valueOne = parseInt(inputOne.value);
+  var valueTwo = parseInt(inputTwo.value);
+  var answer = valueOne / valueTwo;
+  outputArea.innerText = answer;
   if (isNaN(answer) === true) {
     alert('Please enter a number.');
   }
